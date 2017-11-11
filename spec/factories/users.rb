d7a377112cc6002ba306cf9b6668104145cd4857
Bill Tihen <btihen@gmail.com>
2017-11-11 19:47:12 +0100

    added the users scaffold

---
+++ spec/factories/users.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :user do
+    username "MyUsername"
+    full_name "MyName"
+    role "MyRole"
+  end
+end
---
+++ spec/views/users/new.html.erb_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe "users/new", type: :view do
+  before(:each) do
+    assign(:user, User.new(
+      :username => "MyUsername",
+      :full_name => "MyFullName",
+      :role => "MyRole"
+    ))
+  end
+
+  it "renders new user form" do
+    render
+
+    assert_select "form[action=?][method=?]", users_path, "post" do
+
+      assert_select "input[name=?]", "user[username]"
+
+      assert_select "input[name=?]", "user[full_name]"
+
+      assert_select "input[name=?]", "user[role]"
+    end
+  end
+end
