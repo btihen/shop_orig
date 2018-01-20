800b0ed579bc689acd788c008ed029e4681e08e4
Bill Tihen <btihen@gmail.com>
2018-01-20 21:42:55 +0100

    fix tax rate setup

--- app/models/tax_category.rb
+++ app/models/tax_category.rb
@@ -5,9 +5,9 @@ class TaxCategory < ApplicationRecord
   validates :tax_category_name, presence: true,
                                 uniqueness: true,
                                 length: { minimum: 2 }
-  validates :tax_category_rate, allow_nil: false,
-                                numericality: {
-                                  greater_than_or_equal_to: 0,
-                                }
-  validates_date  :tax_start_date,    allow_nil: false
+  # validates :tax_category_rate, allow_nil: false,
+  #                               numericality: {
+  #                                 greater_than_or_equal_to: 0,
+  #                               }
+  # validates_date  :tax_start_date,    allow_nil: false
 end
